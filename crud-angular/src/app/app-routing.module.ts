a37fdd17f62bdd361184ad5422825d093741d610
Josimar Minete <josimaminete@gmail.com>
2021-09-19 19:45:25 -0300

    Adicionado configurações para redirecionar o path "" para /cursos e quando entrar em cursos mostrar o modulo com todos cursos cadastrados.

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full', //verificar qual é o caminho , garante que vai remover tudo do link que pode estar errado.
+    redirectTo: 'cursos',//redirecionar caso seja :4200
+  },
+  {
+    path: 'cursos',
+    loadChildren: () =>
+      import('./cursos/cursos.module').then((m) => m.CursosModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
