0bf1a36d43c17eaa2e0ba713549c3777e22c37eb
Josimar Minete <josimaminete@gmail.com>
2022-02-03 21:21:04 -0300

    Revisão de aulas

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    MatToolbarModule, //para importar os recursos da toolbar
   ],
   providers: [],
   bootstrap: [AppComponent],
